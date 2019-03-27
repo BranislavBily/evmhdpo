f16cc7ef9f9edff7c07d774b69d99f608fa49962
Kristián Kičinka <kicinka@spse-po.sk>
2019-03-27 22:31:18 +0100

    Commit po implementácii Prihlásenia Ver: 1.0

--- src/sample/login/LoginController.java
+++ src/sample/login/LoginController.java
@@ -2,10 +2,18 @@ package sample.login;
 
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -16,6 +24,9 @@ public class LoginController implements Initializable {
     private PasswordField passField;
     @FXML
     private TextField textField;
+    @FXML
+    private Label message;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -29,6 +40,27 @@ public class LoginController implements Initializable {
                 passField.setText(oldVal);
             }
         });
+    }
+        /*Funkcia sluziaca na kontrolu zadaných údajov a vytvorenie nevej Stage*/
+    public void login () throws IOException {
+        if (passField.getText().equals("Admin") && textField.getText().equals("Admin")){
+
+            Stage primaryStage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("../vozidla/Vozidla.fxml"));
+            root.setId("pane");
+            primaryStage.setTitle("Evidencia vozidiel MHD");
+            primaryStage.setScene(new Scene(root));
+           // root.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
+            primaryStage.setMaximized(true);
+            primaryStage.setMinHeight(1080);
+            primaryStage.setMinWidth(1920);
+            primaryStage.setFullScreen(true);
+            primaryStage.show();
+
+        }else {
+            message.setText("Prihlasenie Zlyhalo");
+        }
+
     }
 }
 

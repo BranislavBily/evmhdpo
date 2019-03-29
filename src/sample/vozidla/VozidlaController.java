86ab861191e50d560b3107311567f3bc57ebe30f
Kristián Kičinka <kicinka@spse-po.sk>
2019-03-29 20:00:19 +0100

    Pridanie tabulky a spojazdnenie Databázy ,bez funkčného insertu

--- src/sample/vozidla/VozidlaController.java
+++ src/sample/vozidla/VozidlaController.java
@@ -1,15 +1,46 @@
 package sample.vozidla;
 
+import connectivity.ConnectionClass;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import sample.Main;
 
+import javax.swing.*;
+import java.io.DataInput;
 import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
 
-public class VozidlaController {
+public class VozidlaController implements Initializable {
+
+
+
+    /*Vozidla Controller*/
+
+    public TableView<TableModel> tableView;
+    public TableColumn<TableModel,String> evcColumn;
+    public TableColumn<TableModel,String> vodiciColumn;
+    public TableColumn<TableModel, String> stkColumn;
+    public TableColumn<TableModel,String> typColumn;
+    public TableColumn<TableModel,String> stavColumn;
+    public TableColumn<TableModel,String> reklamaColumn;
+    public TableColumn<TableModel,String> spzColumn;
 
     @FXML
     private javafx.scene.control.Label VozidlaButton;
@@ -17,6 +48,15 @@ public class VozidlaController {
     private javafx.scene.control.Label ServisButton;
     @FXML
     private javafx.scene.control.Label VodiciButton;
+
+
+
+
+
+
+
+
+
 
     @FXML
     public void vozidlaClick() {
@@ -103,6 +143,74 @@ public class VozidlaController {
             e.printStackTrace();
         }
     }
+    /*Pridanie Add buttna*/
+
+
+    public void addButton(){
+        try {
+            Stage stageLogin = new Stage();
+            FXMLLoader loader = new FXMLLoader(VozidlaController.class.getResource("../vozidla/Add.fxml"));
+            Parent root = loader.load();
+            root.getStylesheets().addAll(this.getClass().getResource("../style.css").toExternalForm());
+            Scene scene = new Scene(root);
+
+            stageLogin.setTitle("Login");
+            stageLogin.setScene(scene);
+            //stageLogin.setFullScreen(true)
+            stageLogin.setMinHeight(720);
+            stageLogin.setMinWidth(480);
+            stageLogin.show();
+
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    ObservableList<TableModel> observableList= FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        try {
+            ConnectionClass connectionClass = new ConnectionClass();
+            Connection connection=connectionClass.getConnection();
+            Statement statement=connection.createStatement();
+
+            String sql2="SELECT * FROM VOZIDLA;";
+            ResultSet resultSet=statement.executeQuery(sql2);
+
+            while (resultSet.next()){
+                observableList.add(new TableModel(resultSet.getString("evc"),resultSet.getString("spz"),resultSet.getString("vodic"),resultSet.getString("stk"),resultSet.getString("typVozidla"),resultSet.getString("stavVozidla"),resultSet.getString("reklama")));
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        evcColumn.setCellValueFactory(new PropertyValueFactory<>("tableEvc"));
+        spzColumn.setCellValueFactory(new PropertyValueFactory<>("tableSpz"));
+        vodiciColumn.setCellValueFactory(new PropertyValueFactory<>("tableVodici"));
+        stkColumn.setCellValueFactory(new PropertyValueFactory<>("tableStk"));
+        typColumn.setCellValueFactory(new PropertyValueFactory<>("tableTyp"));
+        stavColumn.setCellValueFactory(new PropertyValueFactory<>("tableStav"));
+        reklamaColumn.setCellValueFactory(new PropertyValueFactory<>("tableReklama"));
+
+        tableView.setItems(observableList);
+
+
+    }
+
+
+    /*Pridanie Zaznamu ADD.fxml*/
+
+
+
+
+
+
+
 }
 
 

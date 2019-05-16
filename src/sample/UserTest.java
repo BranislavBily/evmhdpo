094063dc5c12070c4d7bcb39e669d04da9800d5a
kicinka@spse-po.sk <kicinka@spse-po.sk>
2019-05-16 02:02:00 +0200

    Pridanie základných UnitTestov pre triedu User.

---
+++ src/sample/UserTest.java
@@ -0,0 +1,30 @@
+package sample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+    @Test
+    public void canCreateObjectUser(){
+        User user = new User();
+    }
+
+    @Test
+    public void canSetDataintoObjectUser(){
+        User user = new User();
+        user.setMeno("michal");
+        user.setPassword("heslo");
+        user.setId(2);
+    }
+
+    @Test
+    public void canGetDataintoObjectUser(){
+        User user = new User();
+        System.out.println("username : "+user.getMeno());
+        System.out.println("password : "+user.getPassword());
+        System.out.println("id : "+user.getId());
+    }
+
+
+}

9382b4dc51fbc7716d9e49b46b64f7ddd7f24efa
Branislav Bily <branislav.bily@gmail.com>
2019-06-12 21:43:53 +0200

    Zistuje ci je dany email v databaze

--- src/sample/email/emailFinder/EmailFinder.java
+++ src/sample/email/emailFinder/EmailFinder.java
@@ -1,4 +1,28 @@
-package sample.Email.EmailFinder;
+package sample.email.emailFinder;
+
+import connectivity.ConnectionClass;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmailFinder {
+
+    public static boolean emailInDatabase(String email) {
+        List<Boolean> isEmailCorrect = new ArrayList<>();
+        ConnectionClass connectionClass = new ConnectionClass();
+        Connection connection = connectionClass.getConnection();
+        String query = "SELECT * FROM users WHERE email = ?";
+        try(PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, email);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return resultSet.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }

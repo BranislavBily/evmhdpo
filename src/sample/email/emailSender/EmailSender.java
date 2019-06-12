85bfc2bbde3ef4ff3987c7024e918956ac5d954a
Branislav Bily <branislav.bily@gmail.com>
2019-06-12 21:43:39 +0200

    Posiela email

--- src/sample/email/emailSender/EmailSender.java
+++ src/sample/email/emailSender/EmailSender.java
@@ -1,4 +1,47 @@
-package sample.Email.EmailSender;
+package sample.email.emailSender;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 public class EmailSender {
+
+    public static void sendVerificationEmail(String receiver, String subject, int code) {
+
+        final String sender = "evmhdpo@gmail.com";
+        final String senderPassword = "W#C#!Gy@eMN8feq&s7HF";
+
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "465");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.socketFactory.port", "465");
+        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+        Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(sender, senderPassword);
+                    }
+                });
+        try {
+
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(sender));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse(receiver)
+            );
+            message.setSubject(subject);
+            message.setContent("Your code is: " + code, "text/html; charset=utf-8");
+
+            Transport.send(message);
+
+            System.out.println("email was send");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
 }

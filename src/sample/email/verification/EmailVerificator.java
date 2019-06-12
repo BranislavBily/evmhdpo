90df99b632311ace49caca8660dc7b8e05fec520
Branislav Bily <branislav.bily@gmail.com>
2019-06-12 21:43:29 +0200

    Verifikuje ci zadany email je emailova adresa

--- src/sample/email/verification/EmailVerificator.java
+++ src/sample/email/verification/EmailVerificator.java
@@ -1,4 +1,14 @@
-package sample.Email.Verification;
+package sample.email.verification;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EmailVerificator {
+
+    public static boolean isEmailCorrect(String email) {
+        String regex = "^[\\w!#$%&'*+\\/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+\\/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
 }

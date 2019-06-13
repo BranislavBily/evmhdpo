947ce4e5d8f7337b77492751c93b8fa5e853abf5
Branislav Bily <branislav.bily@gmail.com>
2019-06-13 19:11:30 +0200

    Posielanie vlastneho emailu vodicovi

--- src/sample/vodici/SendEmailController.java
+++ src/sample/vodici/SendEmailController.java
@@ -1,10 +1,16 @@
 package sample.vodici;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import sample.FeedBack;
+import sample.email.emailSender.EmailSender;
 
-import java.awt.*;
 
-public class SendEmailController {
+public class SendEmailController extends FeedBack {
 
     @FXML
     private TextField textFieldPredmetSpravy;
@@ -12,16 +18,29 @@ public class SendEmailController {
     @FXML
     private TextArea textAreaObsahEmailu;
 
-    void onCreate(String userID) {
-        System.out.println(userID);
+    private String userEmail;
+
+    void onCreate(String userEmail) {
+        this.userEmail = userEmail;
+        System.out.println(userEmail);
     }
 
     @FXML
     private void onPoslatEmail() {
-        if(textAreaObsahEmailu.getText().isEmpty()) {
+        resetFeedback();
+        if(textFieldPredmetSpravy.getText().isEmpty()) {
             System.out.println("Ci pana daj obsah emailu");
+            displayErrorFeedback(textFieldPredmetSpravy);
         } else {
-            
+            EmailSender.sendEmail(userEmail, textFieldPredmetSpravy.getText(), textAreaObsahEmailu.getText());
+            Stage stage = (Stage) textFieldPredmetSpravy.getScene().getWindow();
+            stage.close();
+            System.out.println("Email poslany");
         }
+    }
+
+    private void resetFeedback() {
+        DropShadow dropShadow = new DropShadow(0, Color.color(0, 0, 0));
+        textFieldPredmetSpravy.setEffect(dropShadow);
     }
 }
